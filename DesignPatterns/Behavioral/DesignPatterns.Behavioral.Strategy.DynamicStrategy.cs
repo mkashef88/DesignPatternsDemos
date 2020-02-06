dec55ce668115960d2c799429dec79fb0b1399f9
Mahmoud Abdelghfar <mabdelghfar@thiqah.sa>
2020-02-06 15:32:02 +0300

    Strategy Pattern

---
+++ DesignPatterns/Behavioral/DesignPatterns.Behavioral.Strategy.DynamicStrategy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy.DynamicStrategy
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+
+        public void End(StringBuilder sb)
+        {
+        }
+
+        public void Start(StringBuilder sb)
+        {
+           
+        }
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"    <li>{item}</li>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+    }
+
+    public class TextProcessor
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy;
+
+        public void SetOutputFormat(OutputFormat format)
+        {
+            switch (format)
+            {
+                case OutputFormat.Markdown:
+                    listStrategy = new MarkdownListStrategy();
+                    break;
+                case OutputFormat.Html:
+                    listStrategy = new HtmlListStrategy();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+                listStrategy.AddListItem(sb, item);
+            listStrategy.End(sb);
+        }
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+    class DynamicStrategy
+    {
+        public static void Main232(string[] args)
+        {
+            var tp = new TextProcessor();
+            tp.SetOutputFormat(OutputFormat.Markdown);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+
+            tp.Clear();
+            tp.SetOutputFormat(OutputFormat.Html);
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/DesignPatterns.Behavioral.Strategy.StaticStrategy.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral
+{
+     
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+
+        public void End(StringBuilder sb)
+        {
+        }
+
+        public void Start(StringBuilder sb)
+        {
+           
+        }
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"    <li>{item}</li>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+    }
+
+    public class TextProcessor<LS> where LS:IListStrategy, new()
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy=new LS();
+
+        
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+                listStrategy.AddListItem(sb, item);
+            listStrategy.End(sb);
+        }
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+    class DynamicStrategy
+    {
+        public static void Main(string[] args)
+        {
+            var tp = new TextProcessor<MarkdownListStrategy>();
+           
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+
+            var tp2 = new TextProcessor<HtmlListStrategy>();
+           
+            tp2.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp2);
+        }
+    }
+}
